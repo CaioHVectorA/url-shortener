a6c6217c0ed5b9ef66fd60b41ea69a4627202799
CaioHVectorA <caihebatista@gmail.com>
2025-02-22 23:43:21 -0300

    Add services

---
+++ src/services/short.ts
@@ -0,0 +1,4 @@
+// todo
+export function shortenURL({ url, userId }: { url: string; userId: number }) {}
+
+export function getShortenedURL({ short }: { short: string }) {}
---
+++ src/services/user.ts
@@ -0,0 +1,18 @@
+// TODO
+export function registerUser({
+  name,
+  email,
+  password,
+}: {
+  name: string;
+  email: string;
+  password: string;
+}) {}
+
+export function loginUser({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {}
